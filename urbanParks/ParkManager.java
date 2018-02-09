55b2bb6b58c84320094e9d1d623467a458bbe81b
DeepjotKaur <deepjotk@uw.edu>
2018-02-09 13:56:04 -0800

    Add files via upload

--- urbanParks/ParkManager.java
+++ urbanParks/ParkManager.java
@@ -1,11 +1,13 @@
 /**
  * 
  */
-package urbanParks;
+package model;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -22,22 +24,21 @@ public class ParkManager extends User {
 	private int MAX_JOBS = 20;
 	private int MAX_DISTANCE = 75;
 	
-	private Date d = new Date();
-	private int currentDate = d.getDate();
 	
-	/*Business Rule: There can be more than the maximum 
-	 * number of pending jobs at a time in the entire system
-	 *  Check the job class to see how many jobs there are 
-	 *  @return true is there are  exactly max jobs in system.
-	 */
-	public boolean isMaxJobAmountReached() throws FileNotFoundException { //check system for the amount of jobs. 
-		/*1. Parse through the text file and count the amount of jobs in the system.*/
-		File jobFile = new File("Jobs.txt");
-		
-		
-		return false;
-		
-	}
+	
+//	/*Business Rule: There can be more than the maximum 
+//	 * number of pending jobs at a time in the entire system
+//	 *  Check the job class to see how many jobs there are 
+//	 *  @return true is there are  exactly max jobs in system.
+//	 */
+//	public boolean isMaxJobAmountReached() throws FileNotFoundException { //check system for the amount of jobs. 
+//		/*1. Parse through the text file and count the amount of jobs in the system.*/
+//		File jobFile = new File("Jobs.txt");
+//		
+//		
+//		return false;
+//		
+//	}
 	
 	/*Business Rule:
 	 *  No job can be specified that takes more than the maximum 
@@ -45,9 +46,10 @@ public class ParkManager extends User {
 	 *  @param cadidateJob- 
 	 *  @return true if the job will take under max specified days
 	 */
-	public boolean isMaxDaysUnder(Job candidateJob) { 
+	public boolean isMaxDaysUnder(Job theCandidateJob) { 
+		int amountDays = ChronoUnit.DAYS.between(theCandidateJob.getStartDate(),theCandidateJob.getEndDate());
 		
-		if (candidateJob.getLength() > MAX_LENGTH) {
+		if (amountDays > MAX_LENGTH) {
 			return false;
 		}
 		return true;
@@ -58,15 +60,14 @@ public class ParkManager extends User {
 	 * @param cadidateJob- a prospective job.
 	 * @return true if it is over max days away then return false else return true.
 	 */
-	public boolean isJobTooFar(Job candidateJob) {
+	public boolean isJobTooFar(Job theCandidateJob) {
 		
-		Calendar cal = Calendar.getInstance();
-		cal.add(currentDate, MAX_DISTANCE);
-		Date farthestDate = cal.getTime();
-		if(candidateJob.getStartDate() > farthestDate.getDate()) {
+		
+		LocalDate farthestDate = LocalDate.now().plusDays(MAX_DISTANCE);
+		if(ChronoUnit.DAYS.between(farthestDate, theCandidateJob.getStartDate()) > 0) {
 			//a job is more than 75 days into the future- FALSE
 			return false;
-		} else if (candidateJob.getStartDate()<=farthestDate.getDate()) {
+		} else {
 			return true;
 		}
 		return false; 
