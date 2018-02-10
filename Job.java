5f347f2150de32d941d98a8992d22a08f4aafc4e
DeepjotKaur <deepjotk@uw.edu>
2018-02-10 13:43:04 -0800

    Cleaned up code.

--- Job.java
+++ Job.java
@@ -1,20 +1,15 @@
 package model;
 
-import java.awt.List;
 import java.io.Serializable;
-import java.util.ArrayList;
-
-import java.util.Date;
-
 import java.time.LocalDate;
-
+/**
+ * This class holds methods for the Job class.
+ * @author Deepjot Luke 
+ * @date 2.10
+ */
 public class Job  implements Serializable{	
 
-	
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 
@@ -31,17 +26,8 @@ public class Job  implements Serializable{
 
     private LocalDate endDate;
 
-//    private int numberVolsNeeded;
-
-
-
-  ////List of Volunteers for Job
-
 //    public ArrayList<User> Volunteers = new ArrayList<User>();
 
-    public Job() {
-    	//empty constructor
-    }
 
     public Job(String theTitle, String theDescription, String theLocation, LocalDate theStartDate, LocalDate theEndDate) {
     	title = theTitle;
@@ -51,86 +37,36 @@ public class Job  implements Serializable{
     	endDate = theEndDate;
     }
 
-
-
-
-
     public void setJobTitle(String theTitle){
-
         this.title = theTitle;
-
     }
-
-    
 
     public String getTitle(){
-
     	return this.title;
-
     }
-
-    
-
-    
-
-    
 
     public void setDescription(String theDescription){
-
     	this.description = theDescription;
-
     }
-
-    
 
     public String getDescription(){
-
     	return this.description;
-
     }
-
-    
-
-    
-
-    
-
-    
 
     public void setLocation(String theLocation){
-
     	this.location = theLocation;
-
     }
-
-    
 
     public String getLocation(){
-
     	return this.location;
-
     }
-
     
-
-    
-
-    
-
-    
-
     public void setStartDate(LocalDate theStartDate){
-
     	this.startDate = theStartDate;
-
     }
-
-    
-
+ 
     public LocalDate getStartDate(){
-
     	return this.startDate;
-
     }
 
     @Override
@@ -139,64 +75,11 @@ public class Job  implements Serializable{
 				getEndDate() + " Description: " + getDescription();
     }
 
-    
-
-    
-
-    
-
     public void setEndDate(LocalDate theEndDate){
-
     	this.endDate = theEndDate;
-
     }
-
-    
 
     public LocalDate getEndDate(){
-
     	return this.endDate;
-
     }
-
-//    private void setStartDate(Date theDate){
-
-//    	if (theDate < currentDate + 74 Days){
-
-//    		this.startDate = theDate;
-
-//    	}
-
-//    	else {
-
-//    		System.out.println("Not Valid");
-
-//    	}
-
-//        
-
-//    }
-
-//    private void setEndDate(Date theDate){
-
-//       
-
-//        if (theDate < currentDate + 74 Days){
-
-//    		this.endDate = theDate;
-
-//    	}
-
-//    	else {
-
-//    		System.out.println("Not Valid");
-
-//    	}
-
-//    }
-
-
-
-
-
 }
