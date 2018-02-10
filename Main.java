ca95e1fcfaeed457dc8a44260e17496df7842e4a
DeepjotKaur <deepjotk@uw.edu>
2018-02-09 16:27:32 -0800

    Main Class

---
+++ Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package user_interface;
+
+/**
+ * @author deepjot
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Driver.signIn();
+	}
+
+}
