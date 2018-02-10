7d884a6ff8f63779c2f1e89274ce50ace7097f88
TCSS360G1 <35518520+TCSS360G1@users.noreply.github.com>
2018-02-10 15:44:43 -0800

    Cleaned up Test, commented Volunteer exceptions.

--- ScheduleConflictException.java
+++ ScheduleConflictException.java
@@ -1,5 +1,11 @@
 package model;
 
+/**
+ * This exception is thrown when a job is signed for that would conflict
+ * with the schedule of a current job.
+ * @author Kai Stansfield
+ * @version February 10, 2018
+ */
 public class ScheduleConflictException extends Exception{
     private static final long serialVersionUID = -3063427133183512410L;
 
