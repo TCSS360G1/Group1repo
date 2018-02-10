366682b04322319be60d6bbf135ca23b098353ba
TCSS360G1 <35518520+TCSS360G1@users.noreply.github.com>
2018-02-09 16:28:20 -0800

    Add files via upload

---
+++ MinimumDaysException.java
@@ -0,0 +1,10 @@
+package model;
+
+public class MinimumDaysException extends Exception {
+    private static final long serialVersionUID = 3785387344814998707L;
+
+    public MinimumDaysException() {
+        super();
+    }
+
+}
---
+++ ScheduleConflictException.java
@@ -0,0 +1,10 @@
+package model;
+
+public class ScheduleConflictException extends Exception{
+    private static final long serialVersionUID = -3063427133183512410L;
+
+    public ScheduleConflictException() {
+        super();
+    }
+
+}
