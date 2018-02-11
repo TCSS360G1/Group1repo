f5615845aad37b1793224b5394df18f8b3a6b8fc
DeepjotKaur <deepjotk@uw.edu>
2018-02-10 19:39:41 -0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 package user_interface;
 
-
-
 import java.io.File;
 
 import java.io.FileInputStream;
@@ -17,154 +15,145 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 import java.io.Serializable;
-
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 import java.util.Scanner;
 
+import javax.print.attribute.standard.JobSheets;
+
 import model.ParkManager;
+
 import model.User;
+import model.Volunteer;
+import model.Job;
 
-
-
-
-
-public class Main implements Serializable{
-
-	
+public class Main implements Serializable {
 
 	private static ArrayList<User> Users = new ArrayList<User>();
-
-
-
+	private static ArrayList<Job> Jobs = new ArrayList<Job>();
+	
 	public static void main(String[] args) {
-
-
-
-//		ArrayList<User> Users = new ArrayList<User>();
-
-		ParkManager tempManager = new ParkManager();
-
-		tempManager.setName("Luke Manca");
-
-		System.out.println(tempManager.getName());
-
-		Users.add(tempManager);
-
+		fillCollections();
 		System.out.println(Users.toString());
-
-		
-
+		System.out.println(Jobs.toString());
 		serializeUserCollection();
 
-		
-
 		Users.clear();
-
-		
-
+		Jobs.clear();
 		readInUserCollection();
-
-		
 
 		System.out.println(Users.toString());
 
+	}
+	
+	private static void fillCollections() {
+		ParkManager tempManager = new ParkManager("Jenzel", "Villanueva");
+		Users.add(tempManager);
+		ParkManager tempManager1 = new ParkManager("Deepjot", "Kaur");
+		Users.add(tempManager1);
+		Volunteer tempVol1 = new Volunteer("Luke", "Manca");
+		Users.add(tempVol1);
+		Volunteer tempVol2 = new Volunteer("Kai", "Stansfield");
+		Users.add(tempVol2);
 		
-
-}
-
+		Job job1 = new Job("Gas Works Park", "Pick up trash", "Seattle", 
+				LocalDate.of(2018, 3, 4),LocalDate.of(2018, 3, 4));
+		Jobs.add(job1);
+		Job job2 = new Job("Alki Beach", "Picking up litter", "Seattle",
+				LocalDate.of(2018, 3, 19),LocalDate.of(2018, 3, 20));
+		Jobs.add(job2);
+		Job job3 = new Job("Seahurst Beach", "Cleaning trails", "Burien", 
+				LocalDate.of(2018, 2, 19),LocalDate.of(2018, 2, 20));
+		Jobs.add(job3);
+		Job job4 = new Job("Lake Meridian", "Trimming Trees and shrubs, "
+				+ "mowing the grass", "Kent", LocalDate.of(2018, 2, 22),
+				LocalDate.of(2018, 2, 24));
+		Jobs.add(job4);
+		Job job5 = new Job("Wapto Park", "Setting up for events", "Tacoma",
+				LocalDate.of(2018, 2, 13), LocalDate.of(2018, 2, 13));
+		Jobs.add(job5);
+		Job job6 = new Job("Point Defiance Park", "Cleaning trails", "Tacoma", 
+				LocalDate.of(2018, 2, 12), LocalDate.of(2018, 2, 12));
+		Jobs.add(job6);
+		Job job7 = new Job("Jefferson Park", "Set up for events", "Tacoma",
+				LocalDate.of(2018, 2, 13), LocalDate.of(2018, 2, 13));
+		Jobs.add(job7);
+		Job job8 = new Job("Discovery Park", "Clean up beaches", "Seattle", 
+				LocalDate.of(2018, 2, 13), LocalDate.of(2018, 2, 13));
+		Jobs.add(job8);
+	}
 	
+	private static void serializeUserCollection() {
 
+		try {
 
+			FileOutputStream fileOut =
 
-private static void serializeUserCollection(){
+					new FileOutputStream("userFile.ser");
 
-	
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
 
-	
+			out.writeObject(Users);
 
-	try {
+			out.close();
 
-        FileOutputStream fileOut =
+			fileOut.close();
 
-        new FileOutputStream("userFile.ser");
+			System.out.println("Serialized data is saved!");
 
-        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+		} catch (IOException i) {
 
-        out.writeObject(Users);
+			i.printStackTrace();
 
-        out.close();
+		}
 
-        fileOut.close();
+	}
 
-        System.out.println("Serialized data is saved!");
+	private static void readInUserCollection() {
 
-     } catch (IOException i) {
+		// De-serialization
 
-        i.printStackTrace();
+		try
 
-    }
+		{
 
-}
+			// Reading the object from a file
 
+			FileInputStream byteToUsers = new FileInputStream("userFile.ser");
 
+			ObjectInputStream in = new ObjectInputStream(byteToUsers);
 
+			// Method for de-serialization of object
 
+			// REnew users arraylist to a casted serialized object.
 
-private static void readInUserCollection(){
+			Users = (ArrayList<User>) in.readObject();
 
-	 // De-serialization
+			in.close();
 
-    try
+			byteToUsers.close();
 
-    {   
+			System.out.println("Object has been deserialized");
 
-        // Reading the object from a file
+		}
 
-        FileInputStream byteToUsers = new FileInputStream("userFile.ser");
+		catch (IOException ex)
 
-        ObjectInputStream in = new ObjectInputStream(byteToUsers);
+		{
 
-         
+			System.out.println("IOException is caught: De-Serialize");
 
-        // Method for de-serialization of object
+		}
 
-        //REnew users arraylist to a casted serialized object.
+		catch (ClassNotFoundException ex)
 
-        Users = (ArrayList<User>)in.readObject();
+		{
 
-         
+			System.out.println("ClassNotFoundException is caught");
 
-        in.close();
+		}
 
-        byteToUsers.close();
-
-         
-
-        System.out.println("Object has been deserialized");
-
-    }
-
-     
-
-    catch(IOException ex)
-
-    {
-
-        System.out.println("IOException is caught: De-Serialize");
-
-    }
-
-     
-
-    catch(ClassNotFoundException ex)
-
-    {
-
-        System.out.println("ClassNotFoundException is caught");
-
-    }
-
-}
+	}
 
 }
