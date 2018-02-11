c0daa9efa0d7aa92719723897ebaa9d7b8bed337
DeepjotKaur <deepjotk@uw.edu>
2018-02-11 15:27:09 -0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -29,48 +29,12 @@ public class Main implements Serializable {
 		serializeJobCollection();
 		myUsers.clear();
 		myJobs.clear();
-		
-//		readInUserCollection();
-//		readInJobCollection();
+
 	}
 
 	private static void fillCollections() {
 	    readInUserCollection();
         readInJobCollection();
-//		ParkManager tempManager1 = new ParkManager("Jenzel", "Villanueva");
-//		myUsers.add(tempManager1);
-//		ParkManager tempManager2 = new ParkManager("Deepjot", "Kaur");
-//		myUsers.add(tempManager2);
-//		Volunteer tempVol1 = new Volunteer("Luke", "Manca");
-//		myUsers.add(tempVol1);
-//		Volunteer tempVol2 = new Volunteer("Kai", "Stansfield");
-//		myUsers.add(tempVol2);
-//
-//		Job job1 = new Job("Gas Works Park", "Pick up trash", "Seattle",
-//				LocalDate.of(2018, 3, 4), LocalDate.of(2018, 3, 4));
-//		myJobs.add(job1);
-//		Job job2 = new Job("Alki Beach", "Picking up litter", "Seattle",
-//				LocalDate.of(2018, 3, 19), LocalDate.of(2018, 3, 20));
-//		myJobs.add(job2);
-//		Job job3 = new Job("Seahurst Beach", "Cleaning trails", "Burien",
-//				LocalDate.of(2018, 2, 19), LocalDate.of(2018, 2, 20));
-//		myJobs.add(job3);
-//		Job job4 = new Job("Lake Meridian", "Trimming Trees and shrubs, "
-//				+ "mowing the grass", "Kent", LocalDate.of(2018, 2, 22),
-//				LocalDate.of(2018, 2, 24));
-//		myJobs.add(job4);
-//		Job job5 = new Job("Wapto Park", "Setting up for events", "Tacoma",
-//				LocalDate.of(2018, 2, 13), LocalDate.of(2018, 2, 13));
-//		myJobs.add(job5);
-//		Job job6 = new Job("Point Defiance Park", "Cleaning trails", "Tacoma",
-//				LocalDate.of(2018, 2, 12), LocalDate.of(2018, 2, 12));
-//		myJobs.add(job6);
-//		Job job7 = new Job("Jefferson Park", "Set up for events", "Tacoma",
-//				LocalDate.of(2018, 2, 13), LocalDate.of(2018, 2, 13));
-//		myJobs.add(job7);
-//		Job job8 = new Job("Discovery Park", "Clean up beaches", "Seattle",
-//				LocalDate.of(2018, 2, 13), LocalDate.of(2018, 2, 13));
-//		myJobs.add(job8);
 	}
 
 	private static void serializeUserCollection() {
