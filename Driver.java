c85e3c8d302731a7219bc08465279f2f0857978d
lukemanca <lukemanca@gmail.com>
2018-02-21 10:52:49 -0800

    CHECK IN 3
    Method to remove a job specified by the Park Manager

--- Driver.java
+++ Driver.java
@@ -1,132 +1,361 @@
-package model;
+package user_interface;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.time.LocalDate;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import model.AlreadySignedUpException;
+import model.Job;
+import model.MinimumDaysException;
+import model.ParkManager;
+import model.ScheduleConflictException;
+import model.User;
+import model.Volunteer;
 
-public class Driver implements Serializable{
+/**
+ * The User Interface for the program.
+ * This class will have methods to display to the console.
+ * 
+ * @author Jenzel Villanueva
+ * @version February 11, 2018
+ */
+public class Driver {
 	
-	private static ArrayList<User> Users = new ArrayList<User>();
+	private static final int MAX_JOBS_IN_SYSTEM = 20;
+	static Scanner user = new Scanner(System.in);
 
-	public static void main(String[] args) {
+	/* The initial display. this will decide if the user is a volunteer or a PM
+	 *then will call methods to display the correct menus for type. */
+	
+	public static void signIn(ArrayList<User> theUsers, 
+			ArrayList<Job> theJobs) {
+	    System.out.println("Type 'exit' to exit sign in.\n");
+		System.out.print("-Sign in-\nFirst and last name "
+		                + "(separated by a space): ");
+		
+		String name = user.nextLine();
+		boolean userFound = false;
+		if (name.equals("exit")) {
+		    //Dummy branch
+//			System.out.flush();
+		
+		} else {
+    		for(int i = 0; i<theUsers.size(); i++) {
+    			if (theUsers.get(i).getName().equals(name)) {
+    				userFound = true;
+    				if(theUsers.get(i).getType().equals("Manager")) {
+    					ParkManager myManager = (ParkManager) (theUsers.get(i));
+    					System.out.print("\n-Welcome, Manager: "+ 
+    						theUsers.get(i).getFirstName()+ " " + 
+    					                theUsers.get(i).getLastName() + "\n");
+    					showParkManagerMenu(myManager, theUsers, theJobs);
+    					
+    				} else if(theUsers.get(i).getType().equals("Volunteer")) {
+    					System.out.print("\n-Welcome, Volunteer: "+ 
+    							theUsers.get(i).getFirstName()+ " " + 
+    					                theUsers.get(i).getLastName() + "\n");
+    					Volunteer myVolunteer = (Volunteer) (theUsers.get(i));
+    					showVolunteerMenu(myVolunteer, theUsers, theJobs);
+    				}
+    			}
+    		}
+		}
 
-		ArrayList<User> Users = new ArrayList<User>();
-		ParkManager tempManager = new ParkManager();
-		tempManager.setName("Luke Manca");
-		System.out.println(tempManager.getName());
-		Users.add(tempManager);
-		System.out.println(Users.toString());
+		if (userFound == false && !name.equals("exit")) {
+			System.out.println("Invalid user. Please check the spelling!\n");
+            signIn(theUsers, theJobs);
+		}
+	}
+
+	
+	/**
+	 * Shows Volunteers Menu. 3 options to choose from and shows 
+	 * the users job info.
+	 * 
+	 * @param theVolunteer the Volunteer that has signed in.
+	 */
+	public static void showVolunteerMenu(Volunteer theVolunteer, 
+			ArrayList<User> theUsers, ArrayList<Job> theJobs) {
 		
-		serializeUserCollection();
+		System.out.println("1. View jobs volunteered for.");
+		System.out.println("2. Sign Up for upcoming jobs.");
+		System.out.println("3. Sign Out of account.");
 		
-		Users.clear();
-		
-		readInUserCollection();
-		
-		System.out.println(Users.toString());
-		
-}
+		String choice = user.next();
+		if (choice.equals("1")) {
+			ArrayList<Job> volunteerJobs = theVolunteer.getJobs();
+			if (volunteerJobs.isEmpty()) {
+			    System.out.println("No jobs signed up for!\n");
+			    showVolunteerMenu(theVolunteer, theUsers, theJobs);
+			} else {
+			    for (int i = 0; i < volunteerJobs.size(); i++) {
+			        System.out.println((i+1)+ ". " +
+                                volunteerJobs.get(i).getTitle() + " At: " +
+                                volunteerJobs.get(i).getLocation() +" " +
+                                volunteerJobs.get(i).getStartDate() + " To " +
+                                volunteerJobs.get(i).getEndDate() +
+                                " Description: " +
+                                volunteerJobs.get(i).getDescription());
+			    }
+			    System.out.println();
+			    showVolunteerMenu(theVolunteer, theUsers, theJobs);
+			}
+		} else if (choice.equals("2")) {
+			volunteerSignUpForJob(theVolunteer, theUsers, theJobs);
+		} else if (choice.equals("3")) {
+		    user.nextLine();
+			signIn(theUsers, theJobs);
+		} else {
+			System.out.println("You did not input a valid answer so the "
+					+ "menu will be displayed again.\n");
+			showVolunteerMenu(theVolunteer, theUsers, theJobs);
+		}
+	}
 	
 
-private static void serializeUserCollection(){
+	/**
+	 * Shows Park Manager Menu. 3 options to choose from and shows 
+	 * the current active job info.
+	 * 
+	 * @param theManager the Park Manager that has signed in.
+	 * @param theUsers the Users that are in the system.
+	 * @param theJobs the Jobs that are in the System.
+	 */
+	public static void showParkManagerMenu(ParkManager theManager, 
+			ArrayList<User> theUsers, ArrayList<Job> theJobs) {
+		System.out.println("Please choose from one of the following (1-3): ");
+		
+		System.out.println("1. View current active jobs.");
+		System.out.println("2. Create a new park job.");
+		System.out.println("3. Sign Out of account.");
+		System.out.println("4. Unsubmit a Job.");
+		System.out.println("Please type a number between 1 and 3: ");
+		String choice = user.nextLine();
+		
+		if (choice.equals("1")) {
+			
+			System.out.println("Here are all of the current Jobs in the System,"
+					+ " you will be re-directed to the main menu: ");
+			for (int i = 0; i < theJobs.size(); i++) {
+				
+			    System.out.println((i+1)+ ". " + theJobs.get(i).getTitle()+ 
+			                    " At: " +
+			    			theJobs.get(i).getLocation() +" " +
+			                theJobs.get(i).getStartDate() + " To " +
+			                theJobs.get(i).getEndDate() + " Description: " +
+			                theJobs.get(i).getDescription());
+			} 
+			System.out.println();
+			showParkManagerMenu(theManager, theUsers, theJobs);
+		} else if (choice.equals("2")) {
+			//add all of this to the collection. 
+			if(isJobsAmountLegal(theJobs)) {
+				newParkJob(theManager, theUsers, theJobs);
+			}
+			else {
+				System.out.println("Sorry, we are currently filled with the max"
+						+ " amount of jobs, please check back in tomorrow.");
+			}
+			
+		} else if (choice.equals("4")) {
+			System.out.println("Here are all of the current Jobs in the System,"
+					+ " you will be re-directed to the main menu: ");
+			for (int i = 0; i < theJobs.size(); i++) {
+				
+			    System.out.println((i+1)+ ". " + theJobs.get(i).getTitle()+ 
+			                    " At: " +
+			    			theJobs.get(i).getLocation() +" " +
+			                theJobs.get(i).getStartDate() + " To " +
+			                theJobs.get(i).getEndDate() + " Description: " +
+			                theJobs.get(i).getDescription());
+			} 
+			System.out.println();
+			System.out.println("Enter the Title of the Job you would like to remove:");
+			String removeTitle = user.nextLine();
+			removeJob(theManager, removeTitle, theJobs);
+//			showParkManagerMenu(theManager, theUsers, theJobs);
+		}
+
+			
+	}
+
+
+	/**
+	 * Allows Volunteer to sign up for a job.
+	 * 
+	 * @param theVolunteer the Volunteer that has signed in.
+	 * @param theUsers the Users that are in the System.
+	 * @param theJobs the Jobs that are in the System.
+	 */
+	public static void volunteerSignUpForJob(Volunteer theVolunteer, 
+			ArrayList<User> theUsers, ArrayList<Job> theJobs) {
+		System.out.println("\nHere are all of the jobs that are available. "
+				+ "Please choose which job you want by typing a number.\n");
+		for (int i = 0; i < theJobs.size(); i++) {
+		    System.out.println((i + 1) + ")" + "Location:  " +
+		                theJobs.get(i).getTitle()+ " " + "Date:  " +
+		                theJobs.get(i).getStartDate() + " To " +
+		                theJobs.get(i).getEndDate() + " Description:  " +
+		                theJobs.get(i).getDescription());
+		}
+
+		System.out.println("\n-Which job would you like to sign up for? "
+		                   + "0 to exit to main menu.");
+		int value = user.nextInt() - 1;
+		if (value == -1)
+		    showVolunteerMenu(theVolunteer, theUsers, theJobs);
+		else {
+    		try {
+    		    theVolunteer.addJob(theJobs.get(value));
+    		    System.out.println("-You have successfully "
+    		                       + "signed up for this job-");
+    		} catch (AlreadySignedUpException ex) {
+    		    System.out.println("Sorry, you have already" +
+    		                       " signed up for that job");
+    		    volunteerSignUpForJob(theVolunteer, theUsers, theJobs);
+    		} catch (MinimumDaysException ec) {
+    		    System.out.println("Sorry, that job is too close. We ask " + 
+    		                      "That jobs are signed up for no sooner than "+ 
+    		                      Volunteer.MINIMUM_NUMBER_OF_DAYS_TO_SIGN_UP +
+    		                      " Days out");
+    		    volunteerSignUpForJob(theVolunteer, theUsers, theJobs);
+    		} catch (ScheduleConflictException ed) {
+    		    System.out.println("Sorry, looks like that jobs' "
+    		                       + "dates conflict " +
+    		                       "with one of your current ones.");
+    		    volunteerSignUpForJob(theVolunteer, theUsers, theJobs);
+    		}
+    
+    		showVolunteerMenu(theVolunteer, theUsers, theJobs);
+		}
+	}
 	
 	
-	try {
-        FileOutputStream fileOut =
-        new FileOutputStream("userFile.ser");
-        ObjectOutputStream out = new ObjectOutputStream(fileOut);
-        out.writeObject(Users);
-        out.close();
-        fileOut.close();
-        System.out.println("Serialized data is saved!");
-     } catch (IOException i) {
-        i.printStackTrace();
-    }
+	/**
+	 * Asks for information for a new job. Checks to see if the job
+	 * length is max days or under. Checks to see if the job is not
+	 * too far away, and checks to see how many jobs there are.
+	 * 
+	 * @param theManager the Park Manager that has signed in.
+	 * @param theUsers the Users that are in the system.
+	 * @param theJobs the Jobs that are in the System.
+	 */
+	public static void newParkJob(ParkManager theManager, 
+			ArrayList<User> theUsers, ArrayList<Job> theJobs) {
+		
+		System.out.println("-Please provide a job title-");
+		System.out.print("Job Title: ");
+		String t = user.nextLine();
+		while(t.isEmpty()) {
+			System.out.println("-Please provide a VALID job title-");
+			System.out.print("Job Title: ");
+			t = user.nextLine();
+		}
+		
+		System.out.println("-Please provide a job location-");
+		System.out.print("Job Location: ");
+		String l = user.nextLine();
+		while(l.isEmpty()){
+			System.out.println("-Please provide a VALID job location-");
+			System.out.print("Job Location: ");
+			l = user.nextLine();
+		}
+		
+		System.out.println("-What is the starting date for this job?-");
+		System.out.print("Date (MM/DD/YYYY): ");
+		String s = user.nextLine();
+		while(s.isEmpty()){
+			System.out.println("-Please provide a VALID job starting date-");
+			System.out.print("Date (MM/DD/YYYY): ");
+			s = user.nextLine();
+		}
+		String[] dateArray = s.split("/");
+		LocalDate start = LocalDate.of(Integer.parseInt(dateArray[2]), 
+				Integer.parseInt(dateArray[0]), 
+				Integer.parseInt(dateArray[1]));
+		
+		while(!theManager.isJobNotTooFar(start)) { 
+			
+			//while this is false. ask for a new input
+			System.out.println("This date is too far. Please input a date that "
+					+ "is closer.");
+			System.out.print("Date (MM/DD/YYYY): ");
+			s = user.nextLine();
+			dateArray = s.split("/");
+			start = LocalDate.of(Integer.parseInt(dateArray[2]), 
+					Integer.parseInt(dateArray[0]), 
+					Integer.parseInt(dateArray[1]));
+		}
+
+		System.out.println("-What is the ending date for this job?-");
+		System.out.print("Date (MM/DD/YYYY): ");
+		String e = user.nextLine();
+		while(e.isEmpty()){
+			System.out.println("-Please provide a VALID job ending date-");
+			System.out.print("Date (MM/DD/YYYY): ");
+			e = user.nextLine();
+		}
+		LocalDate end;
+		String[] dateArray2 = e.split("/");
+		end = LocalDate.of(Integer.parseInt(dateArray2[2]), 
+				Integer.parseInt(dateArray2[0]), 
+				Integer.parseInt(dateArray2[1]));
+		while(!theManager.isMaxDaysUnder(start, end)) {
+			System.out.println("Please limit your job to 3 days or under.");
+			System.out.print("Date (MM/DD/YYYY): ");
+			e = user.nextLine();
+			dateArray2 = e.split("/");
+			end = LocalDate.of(Integer.parseInt(dateArray2[2]), 
+					Integer.parseInt(dateArray2[0]), 
+					Integer.parseInt(dateArray2[1]));
+		}
+		
+		System.out.println("-Please provide a job description-");
+		System.out.print("Job Description: ");
+		String d = user.nextLine();
+		while(d.isEmpty()){
+			System.out.println("-Please provide a valid job description-");
+			System.out.print("Job Description: ");
+			d = user.nextLine();
+		}
+		Job newJob = new Job(t,d,l, start, end);
+		System.out.println("-This job has been created-");
+
+		System.out.println(newJob.toString());
+		theJobs.add(newJob);
+		showParkManagerMenu(theManager, theUsers, theJobs);
+	}
+	
+	
+	/**
+	 * If the size of the jobs is greater than the max amount that there can 
+	 * be return false, else return true.
+	 * 
+	 * @param theJobs the Jobs that the User has signed up for.
+	 * @return false if there are too many jobs a Volunteer can carry,
+	 * true otherwise.
+	 */
+	public static boolean isJobsAmountLegal(ArrayList<Job> theJobs) {
+		if(theJobs.size() >= MAX_JOBS_IN_SYSTEM) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public static void removeJob(ParkManager theManager, String theTitle, ArrayList<Job> theJobList){
+//		ArrayList<Job> filled_data_ = new ArrayList<>();
+		for (Job accessedJob : theJobList) {
+			if (accessedJob.getTitle() == theTitle){
+				if(theManager.isJobTooSoonToCancel(accessedJob.getStartDate())){
+					System.out.println("Job too soon to be removed");
+					
+				}else{
+					theJobList.remove(accessedJob);
+					System.out.println("Job has been removed");
+				}
+			}
+		}
+	}
 }
-
-
-private static void readInUserCollection(){
-	 // De-serialization
-    try
-    {   
-        // Reading the object from a file
-        FileInputStream byteToUsers = new FileInputStream("userFile.ser");
-        ObjectInputStream in = new ObjectInputStream(byteToUsers);
-         
-        // Method for de-serialization of object
-        //REnew users arraylist to a casted serialized object.
-        Users = (ArrayList<User>)in.readObject();
-         
-        in.close();
-        byteToUsers.close();
-         
-        System.out.println("Object has been deserialized");
-    }
-     
-    catch(IOException ex)
-    {
-        System.out.println("IOException is caught: De-Serialize");
-    }
-     
-    catch(ClassNotFoundException ex)
-    {
-        System.out.println("ClassNotFoundException is caught");
-    }
-}
-}
-
-//	public void readUserFile(){
-//			//Delimiters used in the CSV file
-//			 String COMMA_DELIMITER = ",";
-//
-//				try {
-//					Scanner scanner = new Scanner(new File("Users.csv"));
-//					//Use Delimiter as COMMA
-//					scanner.useDelimiter(COMMA_DELIMITER);
-//					while(scanner.hasNext())
-//					{
-//							System.out.print(scanner.next()+"   ");
-//					}
-//				} 
-//				catch (FileNotFoundException fe) 
-//				{
-//					fe.printStackTrace();
-//				}
-//				finally
-//				{
-//					scanner.close();
-//				}
-//		}
-//	
-//	public void readJobsFile(){
-//		//Delimiters used in the CSV file
-//		 String COMMA_DELIMITER = ",";
-//
-//			try {
-//				Scanner scanner = new Scanner(new File("Jobs.csv"));
-//				//Use Delimiter as COMMA
-//				scanner.useDelimiter(COMMA_DELIMITER);
-//				while(scanner.hasNext())
-//				{
-//						System.out.print(scanner.next()+"   ");
-//				}
-//			} 
-//			catch (FileNotFoundException fe) 
-//			{
-//				fe.printStackTrace();
-//			}
-//			finally
-//			{
-//				scanner.close();
-//			}
-//	}
-
-//}
--- ParkManagerTest.java
+++ ParkManagerTest.java
@@ -1,51 +1,259 @@
-package model;
+ /**
+ * 
+ */
+package testing;
+
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
+
+import java.util.ArrayList;
+
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
+import model.Job;
+import model.ParkManager;
+
+import user_interface.Driver;
+
+/**
+ * Test for Park Manager class.
+ * 
+ * @author Deepjot Kaur
+ * @version February 11, 2018
+ */
 public class ParkManagerTest {
-
-
-
+	private static final int MAX_DISTANCE = 75;
+	private ParkManager myManager;
+	private Driver myDriver;
+	ArrayList<Job> theJobs_farFewer;
+	private ArrayList<Job> theJobs_MAX;
+	private ArrayList<Job> theJobs_oneFewer;
+	
+	/* There can be more than the maximum number of pending myManagers at a
+	 * time in the entire system, default of 20
+	 */
 	@Before
-	public void setUp() throws Exception {
-	        ParkManager myParkManager = new ParkManager();
-	        Date Today = new Date("currentDay");
+    public void setUp() {
+       myManager = new ParkManager("Deepjot", "Kaur");
+       myDriver = new Driver();
+       
+       theJobs_farFewer = new ArrayList<Job>();
+       theJobs_oneFewer = new ArrayList<Job>();
+       theJobs_MAX = new ArrayList<Job>();
+       
+       //create all of the jobs.
+       Job job1 = new Job("Gas Works Park", "Pick up trash", "Seattle",
+				LocalDate.of(2018, 3, 4), LocalDate.of(2018, 3, 4));
+		theJobs_farFewer.add(job1);
+		theJobs_oneFewer.add(job1);
+		theJobs_MAX.add(job1);
+		
+		Job job2 = new Job("Alki Beach", "Picking up litter", "Seattle",
+				LocalDate.of(2018, 3, 19), LocalDate.of(2018, 3, 20));
+		theJobs_farFewer.add(job2);
+		theJobs_oneFewer.add(job2);
+		theJobs_MAX.add(job2);
+		
+		Job job3 = new Job("Seahurst Beach", "Cleaning trails", "Burien",
+				LocalDate.of(2018, 2, 19), LocalDate.of(2018, 2, 20));
+		theJobs_farFewer.add(job3);
+		theJobs_oneFewer.add(job3);
+		theJobs_MAX.add(job3);
+		
+		Job job4 = new Job("Lake Meridian", "Trimming Trees and shrubs, "
+				+ "mowing the grass", "Kent", LocalDate.of(2018, 2, 22),
+				LocalDate.of(2018, 2, 24));
+		theJobs_farFewer.add(job4);
+		theJobs_oneFewer.add(job4);
+		theJobs_MAX.add(job3);
+		
+		Job job5 = new Job("Wapato Park", "Setting up for events", "Tacoma",
+				LocalDate.of(2018, 2, 13), LocalDate.of(2018, 2, 13));
+		theJobs_farFewer.add(job5);
+		theJobs_oneFewer.add(job5);
+		theJobs_MAX.add(job5);
+		
+		Job job6 = new Job("Point Defiance Park", "Cleaning trails", "Tacoma",
+				LocalDate.of(2018, 2, 12), LocalDate.of(2018, 2, 12));
+		theJobs_farFewer.add(job6);
+		theJobs_oneFewer.add(job6);
+		theJobs_MAX.add(job6);
+		
+		Job job7 = new Job("Jefferson Park", "Set up for events", "Tacoma",
+				LocalDate.of(2018, 2, 13), LocalDate.of(2018, 2, 13));
+		theJobs_farFewer.add(job7);
+		theJobs_oneFewer.add(job7);
+		theJobs_MAX.add(job7);
+		
+		Job job8 = new Job("Discovery Park", "Clean up beaches", "Seattle",
+				LocalDate.of(2018, 2, 13), LocalDate.of(2018, 2, 13));
+		theJobs_farFewer.add(job8);
+		theJobs_oneFewer.add(job8);
+		theJobs_MAX.add(job8);
+		
+		Job job9 = new Job("Kerry Park", "Clean up trash", "Seattle",
+				LocalDate.of(2018, 2, 13), LocalDate.of(2018, 2, 13));
+		theJobs_oneFewer.add(job9);
+		theJobs_MAX.add(job9);
+		
+		Job job10 = new Job("Seahurst Park", "Clean up beaches", "Seattle",
+				LocalDate.of(2018, 2, 13), LocalDate.of(2018, 2, 13));
+		theJobs_oneFewer.add(job10);
+		theJobs_MAX.add(job10);
+		
+		Job job11 = new Job("Magnuson Park", "Clean up beaches", "Seattle",
+				LocalDate.of(2018, 2, 13), LocalDate.of(2018, 2, 13));
+		theJobs_oneFewer.add(job11);
+		theJobs_MAX.add(job11);
+		
+		Job job12 = new Job("Lincoln Park", "Clean up beaches", "Seattle",
+				LocalDate.of(2018, 2, 13), LocalDate.of(2018, 2, 13));
+		theJobs_oneFewer.add(job12);
+		theJobs_MAX.add(job12);
+		
+		Job job13 = new Job("Carkeek Park", "Clean up shrubs", "Seattle",
+				LocalDate.of(2018, 2, 13), LocalDate.of(2018, 2, 13));
+		theJobs_oneFewer.add(job13);
+		theJobs_MAX.add(job13);
+		
+		Job job14 = new Job("WaterFront Park", "Clean up trash", "Seattle",
+				LocalDate.of(2018, 2, 13), LocalDate.of(2018, 2, 13));
+		theJobs_oneFewer.add(job14);
+		theJobs_MAX.add(job14);
+		
+		Job job15 = new Job("Seattle Japanees gard", "Plant trees", "Seattle",
+				LocalDate.of(2018, 2, 13), LocalDate.of(2018, 2, 13));
+		theJobs_oneFewer.add(job15);
+		theJobs_MAX.add(job15);
+		
+		Job job16 = new Job("Olympic Sculpture Park", "Plant trees", "Seattle",
+				LocalDate.of(2018, 2, 13), LocalDate.of(2018, 2, 13));
+		theJobs_oneFewer.add(job16);
+		theJobs_MAX.add(job16);
+		
+		Job job17 = new Job("Mrytle Edwards Park", "paint tables", "Seattle",
+				LocalDate.of(2018, 2, 13), LocalDate.of(2018, 2, 13));
+		theJobs_oneFewer.add(job17);
+		theJobs_MAX.add(job17);
+		
+		Job job18 = new Job("Seaward Park", "Plant trees", "Seattle",
+				LocalDate.of(2018, 2, 13), LocalDate.of(2018, 2, 13));
+		theJobs_oneFewer.add(job18);
+		theJobs_MAX.add(job18);
+		
+		Job job19 = new Job("Kubota", "Clean shrubs", "Seattle",
+				LocalDate.of(2018, 2, 13), LocalDate.of(2018, 2, 13));
+		theJobs_oneFewer.add(job19);
+		theJobs_MAX.add(job19);
+		
+		Job job20 = new Job("Seattle Japanees gard", "Plant trees", "Seattle",
+				LocalDate.of(2018, 2, 13), LocalDate.of(2018, 2, 13));
+		theJobs_MAX.add(job20);
+    }
 
-	        myParkManager.createNewJob();
-	        newEntry.setTitle("DayUnder");
-	        
-
-
-	        myParkManager.createNewJob();
-	        newEntry2.setTitle("DayOf");
-	        
-
-
-	        myParkManager.createNewJob();
-	        newEntry3.setTitle("DayOver");
-	        
-
-	    }
-
+	/* test far fewer jobs */
+	@Test
+	public void isJobAmountLegal_FarFewer_true() {
+		assertTrue(myDriver.isJobsAmountLegal(theJobs_farFewer));
 	}
 
+	/* test one fewer jobs */
 	@Test
-	public void testOneDayUnderLimit(){
-		assertTrue(newEntry.setStartDay(Today + 73 Days));
-		} 
-		
-		
-	@Test
-	public void testDayOfLimit(){
-			assertTrue(newEntry2.setStartDay(Today + 74 Days));
-			}
+	public void isMaxmyManagerAmountReached_OneFewer_true() {
+		assertTrue(myDriver.isJobsAmountLegal(theJobs_oneFewer));
+	}
 	
+	/* test exactly 20 jobs */
 	@Test
-	public void testOneDayOverLimit(){
-			assertFalse(newEntry3.setStartDay(Today + 75 Days));
-			}
+	public void isMaxmyManagerAmountReached_Max_false() {
+		assertFalse(myDriver.isJobsAmountLegal(theJobs_MAX));
+	}
+	
+	
+	/*No myManager can be specified that takes more than the maximum number of 
+	 * days, default of 3*/
+	/*test max number 3*/
+	@Test
+	public void isMaxDaysUnder_Max_true() {
+		assertTrue(myManager.isMaxDaysUnder(LocalDate.of(2018, 3, 9), 
+				LocalDate.of(2018, 3, 9)));
+	}
 
+	/* test under 3 days */
+	@Test
+	public void isMaxDaysUnder_under_true() {
+		assertTrue(myManager.isMaxDaysUnder(LocalDate.of(2018, 3, 9), 
+				LocalDate.of(2018, 3, 9)));
+	}
+
+	/* test above max number 3 days */
+	@Test
+	public void isMaxDaysUnder_over_false() {
+		assertFalse(myManager.isMaxDaysUnder(LocalDate.of(2018, 3, 9), 
+				LocalDate.of(2018, 3, 15)));
+	}
+	
+	/* test if job date is too far */
+	@Test
+	public void isJobTooFar_OneFar_false() {
+		LocalDate OneFar = LocalDate.now().plusDays(MAX_DISTANCE+1);
+		assertFalse(myManager.isJobNotTooFar(OneFar));
+	}
+	
+	/*test for one fewer faraway job day */
+	@Test
+	public void isJobTooFar_OneFewer_True() {
+		LocalDate OneFewer= LocalDate.now().plusDays(MAX_DISTANCE-1);
+		assertTrue(myManager.isJobNotTooFar(OneFewer));
+	}
+	
+	/* test for if the day is not too far away */
+	@Test
+	public void isJobTooFar_MaxDistance_True() {
+		LocalDate max = LocalDate.now().plusDays(MAX_DISTANCE);
+		assertTrue(myManager.isJobNotTooFar(max));
+		//System.out.println(ChronoUnit.DAYS.between(LocalDate.of
+		//(2018, 9, 03), LocalDate.of(2018, 13, 03)));
+	}
+	
+	/*No myManager can remove a Job that starts within 3 days of current day*/
+	/*test max number 3*/
+	@Test
+	public void isJobRemovable_inTheClear_false() {
+		assertFalse(myManager.isJobTooSoonToCancel(LocalDate.of(2018, 3, 9)));
+	}
+	/*No myManager can remove a Job that starts within 3 days of current day*/
+	/*test max number 3*/
+	@Test
+	public void isJobRemovable_4DaysLeft_false() {
+		assertFalse(myManager.isJobTooSoonToCancel(LocalDate.of(2018, 2, 25)));
+	}
+	/*No myManager can remove a Job that starts within 3 days of current day*/
+	/*test max number 3*/
+	@Test
+	public void isJobRemovable_3DaysLeft_true() {
+		assertTrue(myManager.isJobTooSoonToCancel(LocalDate.of(2018, 2, 24)));
+	}
+	/*No myManager can remove a Job that starts within 3 days of current day*/
+	/*test max number 3*/
+	@Test
+	public void isJobRemovable_1DayLeft_true() {
+		assertTrue(myManager.isJobTooSoonToCancel(LocalDate.of(2018, 2, 22)));
+	}
+		
+	/*No myManager can remove a Job that starts within 3 days of current day*/
+	/*test day OF Job*/
+	@Test
+	public void isJobRemovable_today_true() {
+		assertTrue(myManager.isJobTooSoonToCancel(LocalDate.now()));
+	}
+	
+	/*No myManager can remove a Job that starts within 3 days of current day*/
+	/*testing A job that was scheduled for yesterday*/
+	@Test
+	public void isJobRemovable_yesterdayJob_true() {
+		assertTrue(myManager.isJobTooSoonToCancel(LocalDate.now().minusDays(1)));
+	}
 }
