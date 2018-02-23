b2c662cce728d376bc3a74254a462259e0ec3619
TCSS360G1 <35518520+TCSS360G1@users.noreply.github.com>
2018-02-23 14:19:04 -0800

    Changed to not have job business rules tested.

--- VolunteerTest.java
+++ VolunteerTest.java
@@ -8,95 +8,46 @@ import java.time.LocalDate;
 import org.junit.Before;
 import org.junit.Test;
 
-import model.AlreadySignedUpException;
 import model.Job;
-import model.MinimumDaysException;
-import model.ScheduleConflictException;
 import model.Volunteer;
 	 
 /**
- * Tests important methods in Volunteer to ensure functionality,
- * and to implicitly test addJob.
+ * Tests that addJobs functions, and that isNotSignedUp is functional.
  *
  * @author Kai Stansfield
  * @version February 11, 2018
  */
 public class VolunteerTest {
-	static int NUMBER_OF_DAYS_TO_SIGN_UP = 2;
-
-	static Volunteer volunteerDoesntMatter;
+	static Volunteer myVolunteerDoesntMatter;
 	
-	static Job wayAfter;
-	static Job justOnTheLimit;
-	static Job pastLimit;
-	
-	static Job CurrentJob;
-	static Job startsDuringCurrent;
-	static Job endsDuringCurrent;
-	static Job noConflicts;
+	static int myMinDaysToSignUp;
+    static int myMinDaysToUnvolunteer;
+    
+	static Job myFirstJob;
+	static Job mySecondJob;
 	
 	@Before
 	public void setup() {
-	    volunteerDoesntMatter = new Volunteer("Kai", "Stansfield");
-	    // DM = Doesn't matter -- isMoreThanTwoDays testers.
-	    wayAfter = new Job("DM", "DM", "DM", LocalDate.now().plusDays(
-	            NUMBER_OF_DAYS_TO_SIGN_UP + 15), 
-	            LocalDate.now().plusDays(NUMBER_OF_DAYS_TO_SIGN_UP + 17));
-	    justOnTheLimit = new Job("DM", "DM", "DM",
-	            LocalDate.now().plusDays(NUMBER_OF_DAYS_TO_SIGN_UP), 
-	            LocalDate.now().plusDays(NUMBER_OF_DAYS_TO_SIGN_UP + 2));
-	    pastLimit = new Job("DM", "DM", "DM", LocalDate.now().plusDays(
-	            NUMBER_OF_DAYS_TO_SIGN_UP - 1),
-	            LocalDate.now().plusDays(NUMBER_OF_DAYS_TO_SIGN_UP));
-	    // -- isNoScedhuleConflicts testers.
-	    CurrentJob = new Job("DM", "DM", "DM", LocalDate.now().plusDays(2),
-	            LocalDate.now().plusDays(4));
-	    try {
-            volunteerDoesntMatter.addJob(CurrentJob);
-        } catch (AlreadySignedUpException | MinimumDaysException
-                        | ScheduleConflictException e) {
-            // No code because we know it won't fail, in this instance.
-        }
-	    
-	    startsDuringCurrent = new Job("DM", "DM", "DM",
-	            LocalDate.now().plusDays(3), LocalDate.now().plusDays(5));
-	    endsDuringCurrent = new Job("DM", "DM", "DM",
-	            LocalDate.now().minusDays(1), LocalDate.now().plusDays(3));
-	    noConflicts = new Job("DM", "DM", "DM",
-	            LocalDate.now().plusDays(5), LocalDate.now().plusDays(7));
+	    myVolunteerDoesntMatter = new Volunteer("Kai", "Stansfield");
+	    myMinDaysToSignUp = Job.MINIMUM_NUMBER_OF_DAYS_TO_SIGN_UP;
+	    myMinDaysToUnvolunteer = Job.MINIMUM_NUMBER_OF_DAYS_TO_UNVOLUNTEER;
+	    //DM = Doesnt matter.
+	    myFirstJob = new Job("DM", "DM", "DM",
+	                    LocalDate.now().plusDays(myMinDaysToSignUp + 15), 
+	                    LocalDate.now().plusDays(myMinDaysToSignUp + 17));
+	    mySecondJob = new Job("DM", "DM", "DM", 
+	                    LocalDate.now().plusDays(myMinDaysToSignUp + 15),
+	                    LocalDate.now().plusDays(myMinDaysToSignUp + 17));
+	    myVolunteerDoesntMatter.addJob(myFirstJob);
 	}
 	
 	@Test
-	public void isMoreThanTwoDays_wayAfter_True() {
-		assertTrue(volunteerDoesntMatter.isMoreThanMinimumDays(wayAfter));
+	public void isNotSignedUp_SameJob_False() {
+	    assertFalse(myVolunteerDoesntMatter.isNotSignedUp(myFirstJob));
 	}
 	
 	@Test
-	public void isMoreThanTwoDays_justOnTheLimit_True() {
-		assertTrue(volunteerDoesntMatter.isMoreThanMinimumDays(justOnTheLimit));
-	}
-	
-	@Test
-	public void isMoreThanTwoDays_pastLimit_False() {
-		assertFalse(volunteerDoesntMatter.isMoreThanMinimumDays(pastLimit));
-	}
-
-	@Test
-	public void isNoScheduleConflicts_startsDuringCurrent_False() {
-	    
-	    assertFalse(volunteerDoesntMatter.isNoScheduleConflicts(
-	                    startsDuringCurrent));
-	}
-	
-	@Test
-    public void isNoScheduleConflicts_endsDuringCurrent_False() {
-	    
-	    assertFalse(volunteerDoesntMatter.isNoScheduleConflicts(
-                        endsDuringCurrent));
+    public void isNotSignedUp_DifferentJob_True() {
+        assertTrue(myVolunteerDoesntMatter.isNotSignedUp(mySecondJob));
     }
-	
-	@Test
-	public void isNoScheduleConflicts_noConflicts_True() {
-	    assertTrue(volunteerDoesntMatter.isNoScheduleConflicts(noConflicts));
-	}
 }
