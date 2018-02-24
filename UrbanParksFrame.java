306885c6f88e7f507d1a0029de4ac4e2056a7791
DeepjotKaur <deepjotk@uw.edu>
2018-02-23 16:15:47 -0800

    Add files via upload

---
+++ UrbanParksFrame.java
@@ -0,0 +1,32 @@
+package user_interface;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JFrame;
+
+public class UrbanParksFrame extends JFrame implements Observer, PropertyChangeListener, ActionListener {
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
