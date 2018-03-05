c6acb5bcf5282eb14976b787da610a85230107ce
DeepjotKaur <deepjotk@uw.edu>
2018-03-04 20:55:42 -0800

    Add files via upload

--- ParkManagerPanel.java
+++ ParkManagerPanel.java
@@ -1,17 +1,16 @@
 package user_interface;
 
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Observer;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
@@ -24,7 +23,7 @@ import model.Job;
 import model.JobCollection;
 import model.ParkManager;
 
-public class ParkManagerPanel extends JPanel implements Observer {
+public class ParkManagerPanel extends JPanel implements PropertyChangeListener {
 
 	/**
 	 * 
@@ -33,45 +32,52 @@ public class ParkManagerPanel extends JPanel implements Observer {
 
 	private JPanel myCancellations;
 	private JPanel myNewJobs;
-	
+
 	private ParkManager myManager;
 
 	private JMenuBar myJMenuBar;
 	private JPanel myCurrentJobsPanel;
-	private ArrayList mySystemJobs;
-	
+	private ArrayList<Job> mySystemJobs;
 
 	public ParkManagerPanel(ParkManager theManager, ArrayList<Job> theJobs) {
-		System.out.println(theManager.getFirstName());
+		System.out.println("size:" + theJobs.size());
+		System.out.println("managers job: " + theManager.getJobs());
 		mySystemJobs = theJobs;
 		myManager = theManager;
-		//System.out.println(Job.filterForCancellation(myManager.getJobs()).size());
-		//Job j = new Job("");
-		//myManager.addJob(j);
+		// System.out.println(Job.filterForCancellation(myManager.getJobs()).size());
+		// Job j = new Job("");
+		// myManager.addJob(j);
 		setLayout(new BorderLayout());
-		setPreferredSize(new Dimension(600,600));
+		setPreferredSize(new Dimension(600, 600));
 		myJMenuBar = new MenuBar();
 		add(myJMenuBar, BorderLayout.NORTH);
-		
+///////////////////////////////////////////////////////////////////ADDED LISTENERS HERE!!!
 		myCurrentJobsPanel = new ParkManagerDisplayCurrentJobs(theManager);
+		myNewJobs = new ParkManagerNewJobPanel(myManager);
+		myNewJobs.addPropertyChangeListener(this);
+		myCurrentJobsPanel = new ParkManagerDisplayCurrentJobs(myManager);
+		myCancellations = new ParkManagerCancelJobsPanel(myManager,
+				Job.filterForCancellation(myManager.getJobs()));
+		myCancellations.addPropertyChangeListener(this);
+
 		add(myCurrentJobsPanel, BorderLayout.CENTER);
-		//displayCurrentJobs(myManager);
-		
-		
+		// displayCurrentJobs(myManager);
+
 		setVisible(true);
 	}
-	
+
 	private void addNewJobPanel() {
-		add(myNewJobs, BorderLayout.SOUTH);
+		add(myNewJobs, BorderLayout.CENTER);
 	}
-	
+
 	private void addCurrentJobPanel() {
-		add(myCurrentJobsPanel, BorderLayout.SOUTH);
+		add(myCurrentJobsPanel, BorderLayout.CENTER);
 	}
 
 	private void addCancellationsJobPanel() {
-		add(myCancellations, BorderLayout.SOUTH);		
+		add(myCancellations, BorderLayout.CENTER);
 	}
+
 	public class MenuBar extends JMenuBar {
 
 		/**
@@ -79,115 +85,114 @@ public class ParkManagerPanel extends JPanel implements Observer {
 		 */
 		private static final long serialVersionUID = 1L;
 
-		private JMenuItem mySignOut;
-		private JMenuItem myUpdates;
-		private JMenuItem myNewJob;
-		private JMenuItem myViewCurrent;
-		
+		private JButton mySignOut;
+		private JButton myUpdates;
+		private JButton myNewJob;
+		private JButton myViewCurrent;
+
 		public MenuBar() {
 			super();
-			myNewJobs = new ParkManagerNewJobPanel(myManager);
-			myCurrentJobsPanel = new ParkManagerDisplayCurrentJobs(myManager);
-			myCancellations = new ParkManagerCancelJobsPanel(myManager, Job.filterForCancellation(myManager.getJobs()));
+
 			current();
 			updates();
 			newJob();
 			signOut();
-			
+
 		}
 
 		private void current() {
-			// TODO Auto-generated method stub
-			JMenu current = new JMenu("Jobs");
-			add(current);
-			myViewCurrent = new JMenuItem("View Current Jobs");
+			myViewCurrent = new JButton("View Current Jobs");
 			myViewCurrent.addActionListener(new ActionListener() {
 				@Override
-				public void actionPerformed(final ActionEvent theEvent) { 
-					//currentJobs.setVisible(false);
+				public void actionPerformed(final ActionEvent theEvent) {
+					// currentJobs.setVisible(false);
 					myNewJobs.setVisible(false);
 					myCancellations.setVisible(false);
+					myCurrentJobsPanel = new ParkManagerDisplayCurrentJobs(myManager);
 					myCurrentJobsPanel.setVisible(true);
 					addCurrentJobPanel();
-					
+
 				}
 			});
-			current.add(myViewCurrent);
+			add(myViewCurrent);
 		}
 
-		private void newJob() {
-			// TODO Auto-generated method stub
-			JMenu newJob = new JMenu("NEW");
-			add(newJob);
-			myNewJob = new JMenuItem("Create a new Job");
+		private void newJob() {			
+			myNewJob = new JButton("Create a new Job");
 			myNewJob.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(final ActionEvent theEvent) {
-					
-					
-					
-					if(mySystemJobs.size()==Job.getLegalJobAmount()) {
-						JOptionPane.showMessageDialog(null, 
+
+					if (mySystemJobs.size() == Job.getLegalJobAmount()) {
+						JOptionPane.showMessageDialog(null,
 								"We are sorry, at this time we are not accepting"
-								+ " any jobs please check bck in another time");
+										+ " any jobs please check bck in another time");
 					} else {
 						myCurrentJobsPanel.setVisible(false);
 						myCancellations.setVisible(false);
+						myNewJobs = new ParkManagerNewJobPanel(myManager);
 						myNewJobs.setVisible(true);
 						addNewJobPanel();
 					}
-					
-					//JOptionPane.showMessageDialog(null, "Clicked the new job");
+
+					// JOptionPane.showMessageDialog(null, "Clicked the new
+					// job");
 				}
 			});
-			newJob.add(myNewJob);
+			add(myNewJob);
 		}
 
 		private void updates() {
-			// TODO Auto-generated method stub
-			JMenu cancel = new JMenu("Cancel");
-			add(cancel);
-			myUpdates = new JMenuItem("Update Jobs");
+			
+			myUpdates = new JButton("Update Jobs");
 			myUpdates.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(final ActionEvent theEvent) {
-					
-					//System.out.println(Job.filterForCancellation(myManager.getJobs())).size());
-					
-					if((Job.filterForCancellation(myManager.getJobs())).size() == 0) {
+
+					// System.out.println(Job.filterForCancellation(myManager.getJobs())).size());
+
+					if ((Job.filterForCancellation(myManager.getJobs()))
+							.size() == 0) {
 						myCancellations.setVisible(false);
-						JOptionPane.showMessageDialog(null, "Sorry, you currently do not have any jobs.");
+						JOptionPane.showMessageDialog(null,
+								"Sorry, you currently do not have any jobs.");
 					} else {
 						myNewJobs.setVisible(false);
 						myCurrentJobsPanel.setVisible(false);
+						myCancellations = new ParkManagerCancelJobsPanel(myManager, Job.filterForCancellation(myManager.getJobs()));
 						myCancellations.setVisible(true);
 						addCancellationsJobPanel();
 					}
-					
+
 				}
-			});
-			cancel.add(myUpdates);
+			}); 
+			add(myUpdates);
 		}
 
 		private void signOut() {
-			// TODO Auto-generated method stub
-			JMenu out = new JMenu("SignOut");
-			add(out);
-			mySignOut = new JMenuItem("Sign Out");
+			mySignOut = new JButton("Sign Out");
 			mySignOut.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(final ActionEvent theEvent) {
-					
+
 				}
 			});
-			out.add(mySignOut);
+			add(mySignOut);
 		}
 
 	}
 
 	@Override
-	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
+	public void propertyChange(PropertyChangeEvent evt) {
+		// get the add event and then fire a listener to the sign in panel.
+		if (evt.getPropertyName().equals("Manager add")) {
+			ParkManager manager = (ParkManager) evt.getOldValue();
+			System.out.println("change recieved in pmp");
+			firePropertyChange("Manager add", manager, evt.getNewValue());
+		} else if (evt.getPropertyName().equals("Manager remove")) {
+			ParkManager manager = (ParkManager) evt.getOldValue();
+			firePropertyChange("Manager remove", manager, evt.getNewValue());
+		}
 
 	}
 
