407c7ed264b3982a5f8c913c6bd221ea74676116
lukemanca <lukemanca@gmail.com>
2018-03-04 16:26:59 -0800

    UnVolunteerPanel,VolunteerPanel & VolunteerMenuBar

---
+++ VolunteerMenuBar.java
@@ -0,0 +1,96 @@
+package user_interface;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JMenuBar;
+
+//This class should theoreticlaly be good to go, but hard to know until we have
+//a concrete way to talk to the model, and a uniform look for the UI.
+public class VolunteerMenuBar extends JMenuBar {
+    private static final long serialVersionUID = 1L;
+    
+    private JButton myCurrentJobsButton;
+    private JButton myAvailableJobsButton;
+    private JButton myUnvolunteerButton;
+    private JButton mySignOutButton;
+    private Component[] myVolunteerPanels;
+    
+    public VolunteerMenuBar(Component[] theComponents) {
+        
+        super();
+        myVolunteerPanels = theComponents;
+        currentJobsButton();
+        availableJobsButton();
+        unVolunteerButton();
+        signOutButton();
+        
+    }
+    
+
+    private void currentJobsButton() {
+        myCurrentJobsButton = new JButton("Current Jobs");
+        myCurrentJobsButton.setEnabled(false);
+        this.add(myCurrentJobsButton);
+        myCurrentJobsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent theEvent) {
+                myCurrentJobsButton.setEnabled(false);
+                myAvailableJobsButton.setEnabled(true);
+                myUnvolunteerButton.setEnabled(true);
+                myVolunteerPanels[0].setVisible(true);
+                myVolunteerPanels[1].setVisible(false);
+                myVolunteerPanels[2].setVisible(false);
+                
+            }
+        });
+    }
+
+    private void availableJobsButton() {
+        myAvailableJobsButton = new JButton("Volunteer!");
+        myAvailableJobsButton.setEnabled(false);
+        this.add(myAvailableJobsButton);
+        myAvailableJobsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent theEvent) {
+                myCurrentJobsButton.setEnabled(true);
+                myAvailableJobsButton.setEnabled(false);
+                myUnvolunteerButton.setEnabled(true);
+                myVolunteerPanels[0].setVisible(false);
+                myVolunteerPanels[1].setVisible(true);
+                myVolunteerPanels[2].setVisible(false);
+            }
+        });
+    }
+
+    private void unVolunteerButton() {
+        myUnvolunteerButton = new JButton("Unvolunteer");
+        this.add(myUnvolunteerButton);
+        myUnvolunteerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent theEvent) {
+                myCurrentJobsButton.setEnabled(true);
+                myAvailableJobsButton.setEnabled(true);
+                myUnvolunteerButton.setEnabled(false);
+                myVolunteerPanels[0].setVisible(false);
+                myVolunteerPanels[1].setVisible(false);
+                myVolunteerPanels[2].setVisible(true);
+            }
+        });
+    }   
+    
+    private void signOutButton() {
+        mySignOutButton = new JButton("Sign Out");
+        this.add(mySignOutButton);
+        mySignOutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent theEvent) {
+                myVolunteerPanels[0].setVisible(false);
+                myVolunteerPanels[1].setVisible(false);
+                myVolunteerPanels[2].setVisible(false);
+            }
+        });
+    }
+}
---
+++ VolunteerPanel.java
@@ -0,0 +1,73 @@
+package user_interface;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JButton;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+
+import model.Job;
+import model.JobCollection;
+import model.Volunteer;
+
+//This class basically acts as a second frame for the volunteer.
+//Everything should be setup up here, it might need look added here to have other
+//things work.
+public class VolunteerPanel extends JPanel implements Observer {
+    private static final long serialVersionUID = 1L;
+
+    private static final Dimension DEFAULT_SIZE = new Dimension(500, 250);
+	
+	private Volunteer myVolunteer;
+	private VolunteerMenuBar myMenuBar;
+	private VolunteerCurrentJobsPanel myCurrentJobsPanel;
+	private VolunteerAvailableJobsPanel myAvailableJobsPanel;
+	private VolunteerUnvolunteerPanel myUnvolunteerPanel;
+	
+	
+    public VolunteerPanel(Volunteer theVolunteer, JobCollection theJobs) {
+        super();
+        myVolunteer = theVolunteer;
+        myCurrentJobsPanel = new VolunteerCurrentJobsPanel(myVolunteer);
+        myAvailableJobsPanel = new VolunteerAvailableJobsPanel(myVolunteer, 
+        		Job.filterForVolunteerAvailableJobs(myVolunteer, theJobs.filterPast()));
+        myUnvolunteerPanel = new VolunteerUnvolunteerPanel(myVolunteer, 
+        		Job.filterForVolunteerAvailableJobs(myVolunteer, theJobs.filterPast()));
+        myMenuBar = new VolunteerMenuBar(getComponents());
+        System.out.println(getComponents().toString());
+        setUpPanel();
+    }
+    
+    /**
+     * Sets up the sign in panel.
+     */
+    private void setUpPanel() {
+    	setLayout(new BorderLayout());
+        setPreferredSize(DEFAULT_SIZE);
+        setBackground(Color.ORANGE);
+        add(myMenuBar);
+        
+        add(myCurrentJobsPanel, BorderLayout.CENTER);
+//        add(myAvailableJobsPanel);
+        add(myUnvolunteerPanel);
+        myCurrentJobsPanel.setVisible(true);
+        setVisible(true);
+    }
+
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	
+}
